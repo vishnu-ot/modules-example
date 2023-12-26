c649015d44ac14194ce66aea3d48c59fddf9c39c
Vishnu <vishnu_ot@yahoo.com>
2023-12-26 22:42:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { add, sub, mul, div } = require("./utils");
+
+console.log(add(5, 6), "sum of two numbers");
+
+console.log(mul(5, 6), "multiplication of  two numbers");
+
+console.log(div(8, 2), "division of  two numbers");
+
+console.log(sub(10, 5), "substraction  of  two numbers");
